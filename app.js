ed91f581fa52748b44317f1cdb34b964f37eddcd
John Bauer <johnbauer126@gmail.com>
2018-11-07 17:45:28 -0500

    basic express app up and running

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log(process.env);
+const express = require('express');
+const app = express();
+
+app.get('/', function(req, res){
+  res.send('express app is up and running');
+});
+
+app.listen(3000);
